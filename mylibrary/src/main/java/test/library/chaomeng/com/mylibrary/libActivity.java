0e94291ac9b9cc657bf32a0009e73ea6a48cf7ee
SQSong <SQSong66@163.com>
2018-09-28 18:25:12 +0800

    测试

---
+++ mylibrary/src/main/java/test/library/chaomeng/com/mylibrary/libActivity.java
@@ -0,0 +1,13 @@
+package test.library.chaomeng.com.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class libActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib);
+    }
+}
